19c8d851855001bbabf37a2c221e529f03fd9718
JR-Berg <126100899+JR-Berg@users.noreply.github.com>
2025-01-23 19:06:17 -0700

    Implemented King movement. He take an big steppy.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -78,6 +78,9 @@ public class ChessPiece {
         if(type == PieceType.QUEEN) {
             return queenMoves(board, myPosition, movesList);
         }
+        if(type == PieceType.KING) {
+            return kingMoves(board, myPosition, movesList);
+        }
         //throw new RuntimeException("Not implemented");
         return new ArrayList<>();
     }
@@ -169,6 +172,33 @@ public class ChessPiece {
                     break;
                 }
                 movesList.add(new ChessMove(myPosition, newPos, null)); //null promotion since this is not a pawn
+            }
+        }
+        return movesList;
+    }
+    private Collection<ChessMove> kingMoves(ChessBoard board, ChessPosition myPosition, Collection<ChessMove> movesList){
+        int[] directionRow = {1,-1,1,-1,1,-1,0,0};
+        int[] directionCol = {1,-1,-1,1,0,0,1,-1};
+
+        for (int i = 0; i < 8; i++){ //There are 8 ordered pairs of directions to move in
+            int newRow = myPosition.getRow();
+            int newCol = myPosition.getColumn();
+            boolean steppy = true; //steppy is set to become false at the end of the while loop every time to ensure we only step once in any direction
+            while(steppy){
+                newRow += directionRow[i];
+                newCol += directionCol[i];
+                if(newRow <= 0 || newRow > board.getRowBounds() || newCol <= 0 || newCol > board.getColumnBounds()){
+                    break;
+                }
+                ChessPosition newPos = new ChessPosition(newRow, newCol);
+                if(board.getPiece(newPos) != null){
+                    if(board.getPiece(newPos).getTeamColor() != board.getPiece(myPosition).getTeamColor()){
+                        movesList.add(new ChessMove(myPosition, newPos, null));
+                    }
+                    break;
+                }
+                movesList.add(new ChessMove(myPosition, newPos, null)); //null promotion since this is not a pawn
+                steppy = false;
             }
         }
         return movesList;
