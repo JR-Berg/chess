445f37cf0037901e9e7be58d86b3e32df53e9179
JR-Berg <126100899+JR-Berg@users.noreply.github.com>
2025-01-23 18:30:29 -0700

    Cleaned up pieceMoves function in ChessPiece.java. Will have if/else statements clogging up the function, so I moved the actual logic of the Bishop's movement to its own function.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -70,41 +70,44 @@ public class ChessPiece {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         ArrayList<ChessMove> movesList = new ArrayList<>();
         if(type == PieceType.BISHOP) {
-            /*I want to implement a set of values that can be used to track all possible directions
-            * That this piece can move, so I am gonna make two arrays. One (directionRow) tracks the
-            * vertical direction [so the change made to the piece's Row] and the other (directionCol)
-            * tracks the horizontal direction. Bishops can move diagonally, so the possible values would
-            * be (1,1) for up and right, (-1,-1) for down and left, (1,-1) for up and left, and (-1,1) for
-            * down and right.
-             */
-            int[] directionRow = {1,-1,-1,1};
-            int[] directionCol = {1,1,-1,-1};
-
-            for (int i = 0; i < 4; i++){ //There are 4 ordered pairs of directions to move in
-                int newRow = myPosition.getRow();
-                int newCol = myPosition.getColumn();
-
-                while(true){
-                    newRow += directionRow[i];
-                    newCol += directionCol[i];
-                    if(newRow <= 0 || newRow > board.getRowBounds() || newCol <= 0 || newCol > board.getColumnBounds()){
-                        break;
-                    }
-                    //TODO: Probably need to implement something in the case of taking pieces as well
-                    System.out.printf("(" + newRow + "," + newCol + ")");
-                    ChessPosition newPos = new ChessPosition(newRow, newCol);
-                    if(board.getPiece(newPos) != null){
-                        if(board.getPiece(newPos).getTeamColor() != board.getPiece(myPosition).getTeamColor()){
-                            movesList.add(new ChessMove(myPosition, newPos, null));
-                        }
-                        break;
-                    }
-                    movesList.add(new ChessMove(myPosition, newPos, null)); //null promotion since this is not a pawn
-                }
-            }
-            return movesList;
+            return bishopMoves(board, myPosition, movesList);
         }
-        return new ArrayList<>();
         //throw new RuntimeException("Not implemented");
+        return new ArrayList<>();
+    }
+
+    private Collection<ChessMove> bishopMoves(ChessBoard board, ChessPosition myPosition, Collection<ChessMove> movesList){
+        /*I want to implement a set of values that can be used to track all possible directions
+         * That this piece can move, so I am gonna make two arrays. One (directionRow) tracks the
+         * vertical direction [so the change made to the piece's Row] and the other (directionCol)
+         * tracks the horizontal direction. Bishops can move diagonally, so the possible values would
+         * be (1,1) for up and right, (-1,-1) for down and left, (1,-1) for up and left, and (-1,1) for
+         * down and right.
+         */
+        int[] directionRow = {1,-1,-1,1};
+        int[] directionCol = {1,1,-1,-1};
+
+        for (int i = 0; i < 4; i++){ //There are 4 ordered pairs of directions to move in
+            int newRow = myPosition.getRow();
+            int newCol = myPosition.getColumn();
+
+            while(true){
+                newRow += directionRow[i];
+                newCol += directionCol[i];
+                if(newRow <= 0 || newRow > board.getRowBounds() || newCol <= 0 || newCol > board.getColumnBounds()){
+                    break;
+                }
+                //System.out.printf("(" + newRow + "," + newCol + ")");
+                ChessPosition newPos = new ChessPosition(newRow, newCol);
+                if(board.getPiece(newPos) != null){
+                    if(board.getPiece(newPos).getTeamColor() != board.getPiece(myPosition).getTeamColor()){
+                        movesList.add(new ChessMove(myPosition, newPos, null));
+                    }
+                    break;
+                }
+                movesList.add(new ChessMove(myPosition, newPos, null)); //null promotion since this is not a pawn
+            }
+        }
+        return movesList;
     }
 }
